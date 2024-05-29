b6ec5d42b8b06def19fdb2340839b243df9c7d5c
unknown <guilhermetcarlos@hotmail.com>
2024-05-29 01:36:46 -0300

    atualizando banco de dados e as rotas

--- api-rym/src/app.module.ts
+++ api-rym/src/app.module.ts
@@ -3,10 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UsersService } from './users/users.service';
+import { AlbumsModule } from './albums/albums.module';
+import { ArtistasModule } from './artistas/artistas.module';
+import { SongsModule } from './songs/songs.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { RatingsModule } from './ratings/ratings.module';
 
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), AlbumsModule, ArtistasModule, SongsModule, ReviewsModule, RatingsModule],
   controllers: [AppController],
   providers: [AppService, UsersService],
 })
